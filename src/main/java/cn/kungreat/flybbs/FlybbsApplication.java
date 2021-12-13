8b29e8c3d15bfb291740cfc901c7a29a6fdd2742
kungreat <mydreams2018@outlook.com>
2021-12-13 21:20:47 +0800

    优化

--- src/main/java/cn/kungreat/flybbs/FlybbsApplication.java
+++ src/main/java/cn/kungreat/flybbs/FlybbsApplication.java
@@ -1,5 +1,7 @@
 package cn.kungreat.flybbs;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,9 +9,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan(basePackages = "cn.kungreat.flybbs.mapper")
 public class FlybbsApplication {
+	/*
+	spring 默认使用的日志工具类
+	*/
+	private final static Log LOGGER = LogFactory.getLog(FlybbsApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlybbsApplication.class, args);
+		LOGGER.info("start finish");
 	}
 
 }
