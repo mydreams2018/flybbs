729ba990661bf46522f599858df721815ccd5018
kungreat2021 <kungreat2021@outlook.com>
2022-06-27 15:48:45 +0800

    admin 监控 设置

--- admin/src/main/java/cn/kungreat/admin/Admin.java
+++ admin/src/main/java/cn/kungreat/admin/Admin.java
@@ -1,11 +1,8 @@
 package cn.kungreat.admin;
 
-import com.alibaba.cloud.nacos.NacosDiscoveryProperties;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 
 @EnableAdminServer
 @SpringBootApplication
@@ -15,14 +12,5 @@ public class Admin {
     */
     public static void main(String[] args) {
         SpringApplication.run(Admin.class, args);
-    }
-
-    @Bean
-    @Primary
-    public NacosDiscoveryProperties nacosProperties() {
-        NacosDiscoveryProperties nacosDiscoveryProperties = new NacosDiscoveryProperties();
-        //此处我只改了ip，其他参数可以根据自己的需求改变
-        nacosDiscoveryProperties.setIp("www.kungreat.cn");
-        return nacosDiscoveryProperties;
     }
 }
---
+++ admin/src/main/java/cn/kungreat/admin/config/SecurityConfig.java
@@ -0,0 +1,64 @@
+package cn.kungreat.admin.config;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.UUID;
+
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final AdminServerProperties adminServer;
+
+    public SecurityConfig(AdminServerProperties adminServer) {
+        this.adminServer = adminServer;
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        // 接口层要获取认证对象的时候  不要在这里放行 这里 不会封装认证对象过来
+        web.ignoring().antMatchers("/actuator/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        SavedRequestAwareAuthenticationSuccessHandler successHandler =
+                new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter("redirectTo");
+        successHandler.setDefaultTargetUrl(this.adminServer.getContextPath() + "/");
+
+        http
+                .authorizeRequests()
+                .antMatchers(this.adminServer.getContextPath() + "/assets/**").permitAll()
+                .antMatchers(this.adminServer.getContextPath() + "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage(this.adminServer.getContextPath() + "/login")
+                .successHandler(successHandler)
+                .and()
+                .logout()
+                .logoutUrl(this.adminServer.getContextPath() + "/logout")
+                .and()
+                .httpBasic()
+                .and()
+                .csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .ignoringRequestMatchers(
+                        new AntPathRequestMatcher(this.adminServer.getContextPath() + "/instances", HttpMethod.POST.toString()),
+                        new AntPathRequestMatcher(this.adminServer.getContextPath() + "/instances/*", HttpMethod.DELETE.toString()))
+                .and()
+                .rememberMe()
+                .key(UUID.randomUUID().toString())
+                .tokenValiditySeconds(1209600);
+    }
+
+}
